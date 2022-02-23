85978d1f28debbaec1cf6d6388e17c44833e2744
thesaddest <youngscarlxrd@yandex.by>
2022-02-23 06:37:01 +0300

    refactoring + completed local redux

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,13 +6,14 @@ const MyPosts = (props) => {
     let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 
     let newPost = React.createRef();
+
     let addPost = () =>{
-        props.addPost();
+        props.dispatch({type: 'ADD-POST'});
     }
 
     let onPostChange = () => {
         let text = newPost.current.value;
-        props.updateNewPostText(text);
+        props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text});
     }
 
     return (
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,56 +1,67 @@
-let rerenderEntireTree = () => {
-    console.log('state was changed')
-}
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you', likesCount: 12},
-            {id: 2, message: 'It\'s my first post', likesCount: 11},
-        ],
-        newPostText: 'new post text'
+import {act} from "react-dom/test-utils";
+
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you', likesCount: 12},
+                {id: 2, message: 'It\'s my first post', likesCount: 11},
+            ],
+            newPostText: 'new post text'
+        },
+        dialogsPage:{
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How are you'},
+                {id: 3, message: 'Wassup'}
+            ],
+            dialogs: [
+                {id: 1, name: 'Rutim'},
+                {id: 2, name: 'Temych'},
+                {id: 3, name: 'Nikitka'}
+            ]
+        },
+        sidebar:{
+            friends:[
+                {id: 1, name: 'Rutim'},
+                {id: 2, name: 'Temych'},
+                {id: 3, name: 'Nikitka'}
+            ]
+        }
     },
-    dialogsPage:{
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How are you'},
-            {id: 3, message: 'Wassup'}
-        ],
-        dialogs: [
-            {id: 1, name: 'Rutim'},
-            {id: 2, name: 'Temych'},
-            {id: 3, name: 'Nikitka'}
-        ]
+    _callSubscriber () {
+        console.log('state was changed')
     },
-    sidebar:{
-        friends:[
-            {id: 1, name: 'Rutim'},
-            {id: 2, name: 'Temych'},
-            {id: 3, name: 'Nikitka'}
-        ]
+
+    getState(){
+        return this._state;
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer; //observer pattern
+    },
+
+    dispatch(action) {
+        if(action.type === 'ADD-POST'){
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            };
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
+        } else if(action.type === 'UPDATE-NEW-POST-TEXT'){
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        } else if (action.type === ''){
+
+        }
     }
-
 }
 
-window.state = state;
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
 
-export const subscribe = (observer) =>{
-    rerenderEntireTree = observer; //observer pattern
-}
+window.store = store;
 
-export default state;
+export default store;
