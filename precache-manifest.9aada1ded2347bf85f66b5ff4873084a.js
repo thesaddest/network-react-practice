ee2ee9743633f34a14dbed84bb28432ae8bcd1fd
thesaddest <youngscarlxrd@yandex.by>
2022-04-23 20:48:15 +0200

    Updates

---
+++ precache-manifest.9aada1ded2347bf85f66b5ff4873084a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "48f3f26f938f9358821d",
+    "url": "/network-react-practice/static/css/main.74fa3c8c.chunk.css"
+  },
+  {
+    "revision": "48f3f26f938f9358821d",
+    "url": "/network-react-practice/static/js/main.48f3f26f.chunk.js"
+  },
+  {
+    "revision": "42ccf354072b12f353e8",
+    "url": "/network-react-practice/static/js/1.42ccf354.chunk.js"
+  },
+  {
+    "revision": "b3fa383616953ff82754",
+    "url": "/network-react-practice/static/css/2.42085457.chunk.css"
+  },
+  {
+    "revision": "b3fa383616953ff82754",
+    "url": "/network-react-practice/static/js/2.b3fa3836.chunk.js"
+  },
+  {
+    "revision": "40034cc083197d1ea7b5",
+    "url": "/network-react-practice/static/css/3.2c2e2760.chunk.css"
+  },
+  {
+    "revision": "40034cc083197d1ea7b5",
+    "url": "/network-react-practice/static/js/3.40034cc0.chunk.js"
+  },
+  {
+    "revision": "013ab6b6aacfd338f96d",
+    "url": "/network-react-practice/static/css/4.61a9cd75.chunk.css"
+  },
+  {
+    "revision": "013ab6b6aacfd338f96d",
+    "url": "/network-react-practice/static/js/4.013ab6b6.chunk.js"
+  },
+  {
+    "revision": "96003da043b6b2ab11ec",
+    "url": "/network-react-practice/static/js/5.96003da0.chunk.js"
+  },
+  {
+    "revision": "12207b2bfd29a8f1df74",
+    "url": "/network-react-practice/static/js/runtime~main.12207b2b.js"
+  },
+  {
+    "revision": "66658ebc6dac4ab554db2d5eadf82889",
+    "url": "/network-react-practice/static/media/preLoader.66658ebc.svg"
+  },
+  {
+    "revision": "ded1dfefa58453a6ed66c0306d801f29",
+    "url": "/network-react-practice/static/media/user.ded1dfef.png"
+  },
+  {
+    "revision": "0c70eae73aaafd7390298bdbf8f8c768",
+    "url": "/network-react-practice/index.html"
+  }
+];
